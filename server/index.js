cf5dfa4f79a5843b79568ece92dad29a905cde76
muchad <muchad@muchad.com>
2025-06-10 22:53:50 +0700

    Update index.js
    Heartbeat and ping health check

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
+const http = require('http');
+const express = require('express');
+
 var process = require('process')
-// Handle SIGINT
+
 process.on('SIGINT', () => {
   console.info("SIGINT Received, exiting...")
   process.exit(0)
 })
 
-// Handle SIGTERM
 process.on('SIGTERM', () => {
   console.info("SIGTERM Received, exiting...")
   process.exit(0)
@@ -15,154 +17,205 @@ const parser = require('ua-parser-js');
 const { uniqueNamesGenerator } = require('unique-names-generator');
 
 const PREFIXES = [
-    'adventurous',
-    'affable',
-    'ambitious',
-    'amiable ',
-    'amusing',
-    'brave',
-    'bright',
-    'charming',
-    'compassionate',
-    'convivial',
-    'courageous',
-    'creative',
-    'diligent',
-    'easygoing',
-    'emotional',
-    'energetic',
-    'enthusiastic',
-    'exuberant',
-    'fearless',
-    'friendly',
-    'funny',
-    'generous',
-    'gentle',
-    'good',
-    'helpful',
-    'honest',
-    'humorous',
-    'imaginative',
-    'independent',
-    'intelligent',
-    'intuitive',
-    'inventive',
-    'kind',
-    'loving',
-    'loyal',
-    'modest',
-    'neat',
-    'nice',
-    'optimistic',
-    'passionate',
-    'patient',
-    'persistent',
-    'polite',
-    'practical',
-    'rational',
-    'reliable',
-    'reserved',
-    'resourceful',
-    'romantic',
-    'sensible',
-    'sensitive',
-    'sincere',
-    'sympathetic',
-    'thoughtful',
-    'tough',
-    'understanding',
-    'versatile',
-    'warmhearted',
+    'Anggrek',
+    'Aren',
+    'Asoka',
+    'Aster',
+    'Bakau',
+    'Bakung',
+    'Bambu',
+    'Bugenvil',
+    'Ceri',
+    'Cempaka',
+    'Cendana',
+    'Cengkeh',
+    'Dahlia',
+    'Damar',
+    'Duku',
+    'Durian',
+    'Gaharu',
+    'Jahe',
+    'Jambu',
+    'Jarak',
+    'Jati',
+    'Jeruk',
+    'Kamala',
+    'Krisan',
+    'Kelapa',
+    'Kemiri',
+    'Kenanga',
+    'Kopi',
+    'Kunyit',
+    'Labu',
+    'Lada',
+    'Lavender',
+    'Leci',
+    'Lili',
+    'Lumut',
+    'Mangga',
+    'Mawar',
+    'Melati',
+    'Meranti',
+    'Nangka',
+    'Nanas',
+    'Naga',
+    'Pakis',
+    'Pala',
+    'Pandan',
+    'Pepaya',
+    'Pinus',
+    'Pisang',
+    'Rosela',
+    'Rotan',
+    'Sagu',
+    'Salak',
+    'Serai',
+    'Seruni',
+    'Soka',
+    'Sukun',
+    'Talas',
+    'Teratai',
 ];
 
-const PLANTS = [
-    "apple",
-    "watermelon",
-    "orange",
-    "pear",
-    "cherry",
-    "strawberry",
-    "nectarine",
-    "grape",
-    "mango",
-    "blueberry",
-    "pomegranate",
-    "carambola",
-    "plum",
-    "banana",
-    "raspberry",
-    "mandarin",
-    "jackfruit",
-    "papaya",
-    "kiwi",
-    "pineapple",
-    "lime",
-    "lemon",
-    "apricot",
-    "grapefruit",
-    "melon",
-    "coconut",
-    "avocado",
-    "peach",
-    "corn",
-    "mushroom",
-    "broccoli",
-    "cucumber",
-    "pineapple",
-    "tomato",
-    "swede",
-    "carrot",
-    "pumpkin",
-    "cabbage",
-    "potato",
-    "eggplant",
-    "turnip",
-    "courgette",
-    "onion",
-    "lettuce",
-    "radish",
-    "pea",
-    "asparagus",
-    "celery",
-    "spinach",
-    "beetroot",
-    "bean",
-    "cilantro",
-    "artichoke",
-    "rosemary",
-    "basil",
-    "clove",
-    "olive",
-    "shallot",
-    "turmeric",
-    "garlic",
-    "ginger",
-    "onion",
-    "lemongrass",
-    "chives",
+const VIRTUES = [
+    'Abadi',
+    'Adil',
+    'Agung',
+    'Aman',
+    'Amanah',
+    'Andal',
+    'Anggun',
+    'Arif',
+    'Asri',
+    'Bahagia',
+    'Bakti',
+    'Benar',
+    'Berani',
+    'Berkat',
+    'Bestari',
+    'Bijak',
+    'Budiman',
+    'Cahaya',
+    'Cekatan',
+    'Cemerlang',
+    'Cerdas',
+    'Damai',
+    'Dermawan',
+    'Elok',
+    'Gagah',
+    'Gemilang',
+    'Gembira',
+    'Harmoni',
+    'Hebat',
+    'Hening',
+    'Hormat',
+    'Ikhlas',
+    'Indah',
+    'Jaya',
+    'Jernih',
+    'Jujur',
+    'Juara',
+    'Kuat',
+    'Kukuh',
+    'Lancar',
+    'Luhur',
+    'Makmur',
+    'Megah',
+    'Menawan',
+    'Mulia',
+    'Murni',
+    'Perkasa',
+    'Pesona',
+    'Rahmat',
+    'Rajin',
+    'Ramah',
+    'Riang',
+    'Rupawan',
+    'Sabar',
+    'Sakti',
+    'Saleh',
+    'Santun',
+    'Sehat',
+    'Sempurna',
+    'Sentosa',
+    'Setia',
+    'Syahdu',
+    'Tangguh',
+    'Teduh',
 ];
 
 class SnapdropServer {
 
-    constructor(port) {
+    constructor(server) {
         const WebSocket = require('ws');
-        this._wss = new WebSocket.Server({ port: port });
-        this._wss.on('connection', (socket, request) => this._onConnection(new Peer(socket, request)));
-        this._wss.on('headers', (headers, response) => this._onHeaders(headers, response));
+
+        this._wss = new WebSocket.Server({
+            server: server,
+
+            clientTracking: true,
+            verifyClient: (info, done) => {
+                const origin = info.req.headers.origin;
+                const allowedOrigins = ['https://muchad.com']; 
+
+                if (allowedOrigins.includes(origin)) {
+                    done(true);
+                } else {
+                    console.warn(`Blocked connection from origin: ${origin}`);
+                    done(false, 403, 'Forbidden');
+                }
+            }
+        });
+
+        this._wss.on('connection', (socket, request) => {
+            this._onConnection(new Peer(socket, request));
+        });
+
+        this._wss.on('headers', (headers, response) => {
+            if (!response.headers?.cookie?.includes('peerid=')) {
+                response.peerId = Peer.uuid();
+                headers.push(`Set-Cookie: peerid=${response.peerId}; SameSite=None; Secure`);
+            }
+            headers.push('Access-Control-Allow-Origin: https://muchad.com');
+            headers.push('Access-Control-Allow-Credentials: true');
+        });
 
         this._rooms = {};
 
+        this._healthCheckInterval = setInterval(() => {
+            this._wss.clients.forEach(ws => {
+
+                if (ws.isAlive === false) {
+                    console.log('Terminating dead connection...');
+                    return ws.terminate();
+                }
+
+                ws.isAlive = false;
+                ws.ping(() => {});
+            });
+        }, 15000); 
     }
+
     _initPeer(peer) {
 
     }
+
     _onConnection(peer) {
+
+        peer.socket.isAlive = true;
+
+        peer.socket.on('pong', () => {
+            peer.socket.isAlive = true;
+        });
+
+        peer.socket.on('close', () => {
+            console.log(`Socket closed for peer ${peer.id}, leaving room.`);
+            this._leaveRoom(peer);
+        });
+
         this._joinRoom(peer);
         peer.socket.on('message', message => this._onMessage(peer, message));
         peer.socket.on('error', console.error);
-        this._keepAlive(peer);
 
-        // send displayName
         this._send(peer, {
             type: 'display-name',
             message: {
@@ -174,42 +227,37 @@ class SnapdropServer {
     }
 
     _onHeaders(headers, response) {
+
         if (response.headers.cookie && response.headers.cookie.indexOf('peerid=') > -1) return;
         response.peerId = Peer.uuid();
         headers.push('Set-Cookie: peerid=' + response.peerId + "; SameSite=Strict; Secure");
     }
 
     _onMessage(sender, message) {
-        // Try to parse message 
         try {
             message = JSON.parse(message);
         } catch (e) {
-            return; // TODO: handle malformed JSON
+            return;
         }
 
         switch (message.type) {
             case 'disconnect':
                 this._leaveRoom(sender);
                 break;
-            case 'pong':
-                sender.lastBeat = Date.now();
-                break;
+
         }
 
-        // relay message to recipient
         if (message.to && this._rooms[sender.ip]) {
-            const recipientId = message.to; // TODO: sanitize
+            const recipientId = message.to; 
             const recipient = this._rooms[sender.ip][recipientId];
             delete message.to;
-            // add sender id
             message.sender = sender.id;
             this._send(recipient, message);
             return;
         }
-        // 修改peer的名字
+
         if(message.displayName){
             sender.modifyDisplayName(message.displayName)
-            // notify all other peers new name
             for (const otherPeerId in this._rooms[sender.ip]) {
                 if (otherPeerId === sender.id) continue;
                 const otherPeer = this._rooms[sender.ip][otherPeerId];
@@ -219,15 +267,13 @@ class SnapdropServer {
                 });
             }
         }
-        
     }
 
     _joinRoom(peer) {
-        // if room doesn't exist, create it
         if (!this._rooms[peer.ip]) {
             this._rooms[peer.ip] = {};
         }
-        // notify all other peers
+
         for (const otherPeerId in this._rooms[peer.ip]) {
             if (otherPeerId === peer.id) continue;
             const otherPeer = this._rooms[peer.ip][otherPeerId];
@@ -237,7 +283,6 @@ class SnapdropServer {
             });
         }
 
-        // notify peer about the other peers
         const otherPeers = [];
         for (const otherPeerId in this._rooms[peer.ip]) {
             if (otherPeerId === peer.id) continue;
@@ -249,23 +294,18 @@ class SnapdropServer {
             currentPeerInfo: peer.name
         });
 
-        // add peer to room
         this._rooms[peer.ip][peer.id] = peer;
     }
 
     _leaveRoom(peer) {
-        if (!this._rooms[peer.ip] || !this._rooms[peer.ip][peer.id]) return;
-        this._cancelKeepAlive(this._rooms[peer.ip][peer.id]);
+        if (!peer || !this._rooms[peer.ip] || !this._rooms[peer.ip][peer.id]) return;
 
-        // delete the peer
+        console.log(`Peer ${peer.id} left room ${peer.ip}`);
         delete this._rooms[peer.ip][peer.id];
 
-        peer.socket.terminate();
-        //if room is empty, delete the room
-        if (!Object.keys(this._rooms[peer.ip]).length) {
+        if (Object.keys(this._rooms[peer.ip]).length === 0) {
             delete this._rooms[peer.ip];
         } else {
-            // notify all other peers
             for (const otherPeerId in this._rooms[peer.ip]) {
                 const otherPeer = this._rooms[peer.ip][otherPeerId];
                 this._send(otherPeer, { type: 'peer-left', peerId: peer.id });
@@ -274,65 +314,29 @@ class SnapdropServer {
     }
 
     _send(peer, message) {
-        if (!peer) return;
-        if (this._wss.readyState !== this._wss.OPEN) return;
+        if (!peer || !peer.socket || peer.socket.readyState !== WebSocket.OPEN) return;
         message = JSON.stringify(message);
-        peer.socket.send(message, error => '');
+        peer.socket.send(message, error => {
+            if (error) console.error(`Error sending message to ${peer.id}:`, error);
+        });
     }
 
-    _keepAlive(peer) {
-        this._cancelKeepAlive(peer);
-        var timeout = 30000;
-        if (!peer.lastBeat) {
-            peer.lastBeat = Date.now();
-        }
-        if (Date.now() - peer.lastBeat > 2 * timeout) {
-            this._leaveRoom(peer);
-            return;
-        }
-
-        this._send(peer, { type: 'ping' });
-
-        peer.timerId = setTimeout(() => this._keepAlive(peer), timeout);
-    }
-
-    _cancelKeepAlive(peer) {
-        if (peer && peer.timerId) {
-            clearTimeout(peer.timerId);
-        }
-    }
 }
 
-
-
 class Peer {
-
     constructor(socket, request) {
-        // set socket
         this.socket = socket;
-
-        // set room
         this._setPeerValues(request);
-
-        // set remote ip
         this._setIP(request);
-
-        // set peer id
         this._setPeerId(request)
-        // is WebRTC supported ?
         this.rtcSupported = request.url.indexOf('webrtc') > -1;
-        // set name 
         this._setName(request);
-        // for keepalive
-        this.timerId = 0;
-        this.lastBeat = Date.now();
+
     }
 
     _setPeerValues(request) {
         let params = (new URL(request.url, "http://server")).searchParams;
-
         let room = params.has("room") ? params.get("room").replace(/\D/g, '') : "";
-
         if (room.length == 6) {
             this.room = room;
         } else {
@@ -348,7 +352,6 @@ class Peer {
         } else {
             this.ip = request.connection.remoteAddress;
         }
-        // IPv4 and IPv6 use different values to refer to localhost
         if (this.ip == '::1' || this.ip == '::ffff:127.0.0.1') {
             this.ip = '127.0.0.1';
         }
@@ -358,7 +361,13 @@ class Peer {
         if (request.peerId) {
             this.id = request.peerId;
         } else {
-            this.id = request.headers.cookie.replace('peerid=', '');
+            const cookieHeader = request.headers.cookie || '';
+            const match = cookieHeader.match(/peerid=([^;]+)/);
+            if (match) {
+                this.id = match[1];
+            } else {
+                this.id = Peer.uuid(); 
+            }
         }
     }
 
@@ -368,20 +377,15 @@ class Peer {
 
     _setName(req) {
         let ua = parser(req.headers['user-agent']);
-
-
         let deviceName = '';
-        
         if (ua.os && ua.os.name) {
             deviceName = ua.os.name.replace('Mac OS', 'Mac') + ' ';
         }
-        
         if (ua.device.model) {
             deviceName += ua.device.model;
         } else {
             deviceName += ua.browser.name;
         }
-
         if(!deviceName)
             deviceName = 'Unknown Device';
         let hasCookieDisplayName = req.url.indexOf('lastDisplayName') > -1
@@ -389,11 +393,10 @@ class Peer {
         uniqueNamesGenerator({
             length: 2,
             separator: ' ',
-            dictionaries: [PREFIXES, PLANTS],
+            dictionaries: [PREFIXES, VIRTUES],
             style: 'capital',
             seed: this.id.hashCode()
         })
-
         this.name = {
             model: ua.device.model,
             os: ua.os.name,
@@ -417,27 +420,14 @@ class Peer {
         this.name.displayName = dispalyName
     }
 
-    // return uuid of form xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
     static uuid() {
-        let uuid = '',
-            ii;
+        let uuid = '', ii;
         for (ii = 0; ii < 32; ii += 1) {
             switch (ii) {
-                case 8:
-                case 20:
-                    uuid += '-';
-                    uuid += (Math.random() * 16 | 0).toString(16);
-                    break;
-                case 12:
-                    uuid += '-';
-                    uuid += '4';
-                    break;
-                case 16:
-                    uuid += '-';
-                    uuid += (Math.random() * 4 | 8).toString(16);
-                    break;
-                default:
-                    uuid += (Math.random() * 16 | 0).toString(16);
+                case 8: case 20: uuid += '-'; uuid += (Math.random() * 16 | 0).toString(16); break;
+                case 12: uuid += '-'; uuid += '4'; break;
+                case 16: uuid += '-'; uuid += (Math.random() * 4 | 8).toString(16); break;
+                default: uuid += (Math.random() * 16 | 0).toString(16);
             }
         }
         return uuid;
@@ -450,10 +440,20 @@ Object.defineProperty(String.prototype, 'hashCode', {
     for (i = 0; i < this.length; i++) {
       chr   = this.charCodeAt(i);
       hash  = ((hash << 5) - hash) + chr;
-      hash |= 0; // Convert to 32bit integer
+      hash |= 0; 
     }
     return hash;
   }
 });
 
-const server = new SnapdropServer(process.env.PORT || 3000);
+const app = express();
+app.get('/ping', (req, res) => {
+  console.log('Ping received!');
+  res.status(200).send('OK');
+});
+const port = process.env.PORT || 4000;
+const server = http.createServer(app);
+const snapdropServer = new SnapdropServer(server);
+server.listen(port, () => {
+  console.log(`Server listening on port ${port} for both HTTP and WebSocket`);
+});
